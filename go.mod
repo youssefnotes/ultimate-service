6af19285ab2de161932e4e3e38c003df8826941c
Mohamed Youssef <mohamed_y@msn.com>
2020-06-18 15:29:23 +0800

    read environment file add database connection add database migration

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module ultimate-service
 
 go 1.13
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1 // indirect
+	github.com/GuiaBolso/darwin v0.0.0-20191218124601-fd6d2aa3d244
+	github.com/cznic/ql v1.2.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.0.0
+	google.golang.org/appengine v1.6.6 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,71 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/GuiaBolso/darwin"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 )
 
+var (
+	migrations = []darwin.Migration{
+		{
+			Version:     1,
+			Description: "Creating table products",
+			Script: `CREATE TABLE products (
+						product_id UUID, 
+						name 		TEXT,
+						price 		float8,
+						quantity 	float8,
+						date_created TIMESTAMP,
+						date_updated TIMESTAMP,
+						PRIMARY KEY (product_id)
+					 );`,
+		}}
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 func main() {
-	address := "localhost:8000"
+	log.Println("main: Starting")
+	defer log.Println("main: Completed")
+
+	// ============================================================================================================
+	// setup dependency
+
+	// open database
+	db, err := openDB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("db connected")
+	defer db.Close()
+
+	//migrations
+	err = Migrate(db)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("db migration complete")
+	address := fmt.Sprintf("%s:%s", os.Getenv("app_ip"), os.Getenv("app_port"))
 	if err := http.ListenAndServe(address, http.HandlerFunc(getProducts)); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func Migrate(db *sqlx.DB) error {
+	driver := darwin.NewGenericDriver(db.DB, darwin.PostgresDialect{})
+	d := darwin.New(driver, migrations, nil)
+	return d.Migrate()
 }
 
 type Product struct {
@@ -42,4 +98,19 @@ func getProducts(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	return
+}
+
+func openDB() (*sqlx.DB, error) {
+	q := url.Values{}
+	q.Set("sslmode", os.Getenv("db_ssl_mode"))
+	q.Set("timezone", os.Getenv("db_time_zone"))
+	u := url.URL{
+		Scheme:   os.Getenv("db_scheme"),
+		User:     url.UserPassword(os.Getenv("db_user_name"), os.Getenv("db_pass_word")),
+		Host:     os.Getenv("db_ip"),
+		Path:     os.Getenv("db_path"),
+		RawQuery: q.Encode(),
+	}
+
+	return sqlx.Open(os.Getenv("db_driver_name"), u.String())
 }
